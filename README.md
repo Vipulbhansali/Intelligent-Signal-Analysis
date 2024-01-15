09fd1460a5c71518645a6c05250c3afc42ff0848
Vipulbhansali <vipuljain81492@gmail.com>
2024-01-15 15:29:43 +0530

    This is first version of source code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# neurolab-mongodb-python
+# neurolab-mongo-python
+
+
+
+### Step 1 - Install the requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+### Step 2 - Run main.py file
+
+```bash
+python main.py
+```
---
+++ data_dump.py
@@ -0,0 +1,21 @@
+import pymongo
+import pandas as pd
+import json
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+DATABASE_NAME = 'aps'
+COLLECTION_NAME = 'sensor'
+DATA_FILE_PATH = "/config/workspace/aps_failure_training_set1.csv"
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f'Rows and Columns : {df.shape}')
+
+    #convert dataframe to json to dump this record into mongodb
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    
+     #insert converted json record to mongodb
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
